0f06caf6eac9563aa1991266e355f785cf75fc9e
ezh29 <ezh29@naver.com>
2018-09-04 00:02:30 +0900

    slider1 - slider / slider2 -bxslider

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
 $(window).load(function () {
-
     $("#slider1").mySlider({ //탭박스 이름
         dur: 1000, //속도
         rolling_dur: 2000, //롤링 시간
@@ -13,19 +12,29 @@ $(window).load(function () {
         autoStart: true //자동롤링 시작 유무 true:로딩시 롤링시작 ,  false : 버튼을 클릭해야 롤링시작
     });
 
-
-    $("#slider2").mySlider({ //탭박스 이름
-        dur: 1000, //속도
-        rolling_dur: 2000, //롤링 시간
-        activeName: "on", //활성화시킬 클래스 이름
-        panel: ".panel2", //슬라이더 ul 이름
-        navi: ".navi2", //버튼 ul 이름
-        prev: ".prev2", //이전버튼 이름
-        next: ".next2", //다음버튼 이름
-        start: ".start2", //롤링시작버튼 이름
-        stop: ".stop2", //롤링정지버튼 이름
-        autoStart: true //자동롤링 시작 유무 true:로딩시 롤링시작 ,  false : 버튼을 클릭해야 롤링시작
+    var mySlider = $('.bxslider').bxSlider({
+        mode: 'horizontal', // 가로 방향 수평 슬라이드
+        speed: 500, // 이동 속도를 설정
+        pager: true, // 현재 위치 페이징 표시 여부 설정
+        moveSlides: 1, // 슬라이드 이동시 개수
+        minSlides: 4, // 최소 노출 개수
+        maxSlides: 4, // 최대 노출 개수
+        slideMargin: 5, // 슬라이드간의 간격
+        auto: true, // 자동 실행 여부
+        autoHover: true, // 마우스 호버시 정지 여부
+        controls: true // 이전 다음 버튼 노출 여부
     });
+
+
+    
+
+    var mql = window.matchMedia("screen and (max-width: 300px)");
+    mql.addListener(function (e) {
+        if (!e.matches) {
+            slider.reloadSlider();
+        }
+    });
+
 
 
     //con2 비디오 설정
